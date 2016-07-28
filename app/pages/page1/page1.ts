b1e50197debddcd8cd1d0c453b9c35df5f6264c2
Seth Bunke <seth.bunke@hotmail.com>
2016-07-28 02:00:29 +0000

    added takePicture method - testing needed

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -8,4 +8,23 @@ export class Page1 {
 
   constructor(private navController: NavController) {
   }
+
+  takePicture() {  
+        Camera.getPicture({
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            saveToPhotoAlbum: this.saveToPhotoAlbum,
+            correctOrientation: true  
+        }).then(imageData => {
+            this.base64Image = "data:image/jpeg;base64," + imageData;
+        }, error => {
+            console.log("ERROR -> " + JSON.stringify(error));
+            this.presentAlert('Error', error);
+        });
+    }
 }
