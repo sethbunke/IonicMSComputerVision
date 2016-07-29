ad07dd78a0aa69497bd1b6aaa2671aa5d5bebc17
Seth Bunke <seth.bunke@hotmail.com>
2016-07-29 00:45:57 +0000

    referencing and injecting computer vision service; added logging

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController, Alert } from 'ionic-angular';
+import { ComputerVisionService } from '../../providers/computer-vision-service/computer-vision-service';
 import {Camera} from 'ionic-native';
 
 
 @Component({
-  templateUrl: 'build/pages/page1/page1.html'
+  templateUrl: 'build/pages/page1/page1.html',
+  providers: [ComputerVisionService]
 })
 export class Page1 {
 
-  base64Image: string;
-  saveToPhotoAlbum: boolean;
+    response: any;
+    base64Image: string;
+    saveToPhotoAlbum: boolean;
 
-  constructor(private navController: NavController) {
+  constructor(private navController: NavController, private computerVisionService: ComputerVisionService) {
       this.saveToPhotoAlbum = false;
   }
 
@@ -37,9 +40,31 @@ export class Page1 {
             correctOrientation: true  
         }).then(imageData => {
             this.base64Image = "data:image/jpeg;base64," + imageData;
+            console.log('image captured successfully');
+
         }, error => {
             console.log("ERROR -> " + JSON.stringify(error));
             this.presentAlert('Error', error);
         });
+    }
+    callComputerVisionService() {
+        //verify that the the base64 data is there
+
+        console.log('calling computerVisionService');
+
+        this.computerVisionService.getFacesForImageBase64Data(this.base64Image).subscribe(
+            data => {
+                console.log(data); //comment
+                this.response = JSON.stringify(data.json());
+                let resp = data.json(); 
+                this.presentAlert('Success', 'Successful repsonse from service');
+            },
+                err => {
+                console.log(err);
+                this.response = err;
+                this.presentAlert('Error', err);
+            },
+            () => console.log('Computer Vision Service Call Complete')
+        );
     }
 }
